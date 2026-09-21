a85768fdbd1fb0827e03789bf620eb81fac6a3e3
greatPHPer <dhilip007.india@gmail.com>
2026-09-21 19:04:51 +0530

    Mark journal deletions as historical events

--- FileRecovery/DeletionDetectedEventArgs.cs
+++ FileRecovery/DeletionDetectedEventArgs.cs
@@ -1,6 +1,7 @@
 namespace FileRecovery;
 
-public sealed class DeletionDetectedEventArgs(DeletionRecord record) : EventArgs
+public sealed class DeletionDetectedEventArgs(DeletionRecord record, bool historical = false) : EventArgs
 {
     public DeletionRecord Record { get; } = record;
+    public bool Historical { get; } = historical;
 }
