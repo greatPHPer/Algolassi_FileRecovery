f21cd7c1c5a83e175ed6fcedb3ec61a38820b379
greatPHPer <dhilip007.india@gmail.com>
2026-09-22 07:14:04 +0530

    Track history row identity for selection restore

--- FileRecovery/RecoveryDisplayRow.cs
+++ FileRecovery/RecoveryDisplayRow.cs
@@ -2,6 +2,7 @@ namespace FileRecovery;
 
 public sealed class RecoveryDisplayRow
 {
+    public Guid? HistoryId { get; init; }
     public string Name { get; init; } = string.Empty;
     public string DeletedOn { get; init; } = string.Empty;
     public string FileSize { get; init; } = string.Empty;
